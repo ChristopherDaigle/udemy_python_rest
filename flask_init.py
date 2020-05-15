e702ecfb9dcc66957344c15c01610ded14a6cb83
Chris Daigle <chris.daigle@uconn.edu>
2020-05-15 11:57:53 -0400

    feat: include json

--- flask_init.py
+++ flask_init.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 
@@ -7,5 +7,16 @@ def home():
     return "<h1>Home</h1>"
 
 
+@app.route("/test_json")
+def test_json():
+
+    data = {
+        'field1': 'abc',
+        'field2': 'def'
+    }
+
+    return jsonify(data)
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="127.0.0.1")
